939e4b6ed66cb04926f602f203aa5d6cb4f3d2c5
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-07 16:11:26 -0400

    created a relationship bitween a user and category of pitch

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,  primary_key = True)
     username = db.Column(db.String(255))
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
 
     def __repr__(self):
         return f'User {self.username}'
@@ -14,7 +15,8 @@ class Category(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    description = db.Column(db.String(255)) 
+    description = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'category',lazy="dynamic") 
 
     def __repr__(self):
         return f'User {self.name}'       
