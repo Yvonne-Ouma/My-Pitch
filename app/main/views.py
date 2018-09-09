39ca2b9849a34d3dc4d29060d33445e954563f35
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-09 14:21:27 -0400

    modified my files

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,19 @@
 from flask import render_template
+from flask_login import login_required
+from . import main
+
 app = Flask(__name__)
 
-@app.route("/")
+@main.route("/")
 def index():
-    return render_template('index.html')
+    '''
+    title = "Get Started with a pitch"
+    '''
+    return render_template('index.html' title= title)
+
+    
+    
+    
+
+    
      
