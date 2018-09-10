fee05a6f9764fa7fccbee55c8b20e140ee7881e0
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-10 00:20:20 -0400

    addeda route for the profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from . import main
-from flask import render_template
+from flask import render_template,redirect,url_for,abort
 from flask_login import login_required
-
+from ..models import User 
 
 app = Flask(__name__)
 
@@ -13,6 +13,16 @@ def index():
     '''
     title = 'Get started with a pitch'
     return render_template('index.html', title= title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+
+    return render_template("profile/profile.html", user = user)        
 
 
     
