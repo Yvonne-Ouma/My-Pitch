85819394973eff573d362cc38f62a895ca066b6e
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-14 12:52:18 -0400

    deploying

--- config.py
+++ config.py
@@ -22,14 +22,14 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 # class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/yvonneoumapitch_test'
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/my_pitch_test'
 
-class DevConfig(Config):
-    DEBUG = True
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/yvonneoumapitch'
+# class DevConfig(Config):
+#     DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/my_pitch'
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig,
+# 'development':DevConfig,
+'production':ProdConfig
 # 'test':TestConfig
 }    
