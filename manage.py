33bf613f9bafdd9a4f2491e852d6db54db0d66e1
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-14 12:07:31 -0400

    add the changes

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ from flask_script import Manager,Server
 from app.models import User,Pitch,Comment
 from flask_migrate import Migrate , MigrateCommand
 
+#app = create_app('development')
 app = create_app('production')
-
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('db', MigrateCommand)
