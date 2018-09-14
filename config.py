3be90f7956c1a65e52e526e2701bd937f658cf31
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-14 12:16:43 -0400

    deployement to heroku

--- config.py
+++ config.py
@@ -21,15 +21,15 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/my_pitch_test'
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/yvonneoumapitch_test'
 
-class DevConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/my_pitch'
+# class DevConfig(Config):
+#     DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/yvonneoumapitch'
 
 config_options = {
-'development':DevConfig,
+# 'development':DevConfig,
 'production':ProdConfig,
-'test':TestConfig
+# 'test':TestConfig
 }    
